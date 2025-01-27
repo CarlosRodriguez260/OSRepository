c16a125c5c4aae04b5347f2a1950a80fda8a5d88
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2025-01-27 15:14:20 -0400

    CosineSeries Done

---
+++ project/build/CMakeFiles/cosine_series.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vmboxuser/Documents/OSRepository/project/src/cosineSeries.c" "CMakeFiles/cosine_series.dir/src/cosineSeries.c.o" "gcc" "CMakeFiles/cosine_series.dir/src/cosineSeries.c.o.d"
+  "/home/vmboxuser/Documents/OSRepository/project/src/main.c" "CMakeFiles/cosine_series.dir/src/main.c.o" "gcc" "CMakeFiles/cosine_series.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/build/CMakeFiles/cosine_series.dir/src/main.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/cosine_series.dir/src/main.c.o: \
+ /home/vmboxuser/Documents/OSRepository/project/src/main.c \
+ /usr/include/stdc-predef.h \
+ /home/vmboxuser/Documents/OSRepository/project/include/cosineSeries.h
