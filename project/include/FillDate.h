37144875a06b81897b557b1aafc49e1a4a17aedc
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2025-01-27 18:52:26 -0400

    FillDate Excercise

---
+++ project/include/FillDate.h
@@ -0,0 +1,6 @@
+#ifndef FILLDATE_H
+#define FILLDATE_H
+
+void FillDate(long second, int* day, int* month, int* year);
+
+#endif // FILLDATE_H
---
+++ project/src/FillDate.c
@@ -0,0 +1,10 @@
+#include "FillDate.h"
+#include "math.h"
+#include <stdio.h>
+
+void FillDate(long second, int* day, int* month, int* year){
+    // 1 year = 31,536,000 seconds
+    // 1 month = 2,628,000 seconds
+    // 1 day = 86,400 seconds
+    printf("A");
+}
