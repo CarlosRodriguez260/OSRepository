f5bf73bb7aee7e345044c3d9c9c4dd298f4072d0
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2025-02-02 15:37:53 -0400

    Done

--- project/src/upperRand.c
+++ project/src/upperRand.c
@@ -1,30 +1,31 @@
-#include "header.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <ctype.h>
+#include "header.h"
 
 /**
- * @brief Receives a pointer "base" that points to a char array filled with certain characters. We then
- *        iterate through this char array and randomly choose to uppercase them. This modified char array
- *        gets passed to the pointer of "mod", which points to another char array.
- * 
- * 
- * 
+ * @brief Receives a pointer "base" that points to a char array filled with
+ * certain characters. We then iterate through this char array and randomly
+ * choose to uppercase them. This modified char array gets passed to the pointer
+ * of "mod", which points to another char array.
+ *
+ *
+ *
  * @param base Points to the unmodified character array
  * @param mod Points to where we still store the modified char array
  * @return Nothing, only modifies the values pointed to by the parameters
  */
 
-void UpperRand(char * base, char * mod){
-    int random;
-    for(int i = 0; base[i] != '\0'; i++){
-        char temp = base[i];
-        random = rand();
-        // We only uppercase the letter if the random number is divisible by 2
-        if(random%2==0){
-            temp = toupper(base[i]);
-        }
-        mod[i] = temp;
+void UpperRand(char* base, char* mod) {
+  int random;
+  for (int i = 0; base[i] != '\0'; i++) {
+    char temp = base[i];
+    random = rand();
+    // We only uppercase the letter if the random number is divisible by 2
+    if (random % 2 == 0) {
+      temp = toupper(base[i]);
     }
+    mod[i] = temp;
+  }
 }
