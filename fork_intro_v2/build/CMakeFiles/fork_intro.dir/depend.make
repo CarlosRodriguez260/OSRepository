fbf1ed347791026faae8a613e4e6cb3a08142dc7
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2025-02-05 11:36:51 -0400

    Forking Example Codes

--- fork_intro_v2/build/CMakeFiles/fork_intro.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for fork_intro.
-# This may be replaced when dependencies are built.
--- fork_intro_v2/src/main.c
+++ fork_intro_v2/src/main.c
@@ -5,20 +5,33 @@
 #include <wait.h>
 
 int main() {
-  pid_t pid;
+  int originalPID = getpid();
+  pid_t pid1;
+  pid_t pid2;
+  pid_t pid11;
 
-  pid = fork();
-
-  printf("fork return value %d\n", pid);
-
-  if (pid == 0) {
-    execlp("firefox", "firefox", NULL);
-  } else {
-    printf("hi, I'm the parent, my id is %d\n", getpid());
-    sleep(30);
+  pid1 = fork();
+  if(originalPID == getpid()){
+    pid2 = fork();
   }
+  // Check for first child
+  if(pid1==0){
+    printf("I am the first child. My PID should be +1 to my parent, and it is: %d \n",getpid());
+    //execlp("firefox","firefox",NULL);
+    int originalPID2 = getpid();
+    pid11 = fork();
 
-  printf("I'm done\n");
-
-  return 0;
+    if(pid11==0){
+      printf("I am the child of child %d, and my PID is: %d \n", originalPID2, getpid());
+      while(1){printf("A");}
+    }
+  }
+  // Check for second child
+  else if(pid2==0){
+    printf("I am the second child. My PID should be +2 to my parent, and it is: %d \n",getpid());
+  }
+  // If neither, you are parent
+  else{
+    printf("I am the parent. My PID is: %d \n", getpid());
+  }
 }
