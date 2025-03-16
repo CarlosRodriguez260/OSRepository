a22c4af798e253da0b99de838e017f51b8e8b0ca
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2025-03-16 16:18:48 -0400

    Done

--- bouncing_ball/src/main.c
+++ bouncing_ball/src/main.c
@@ -1,26 +1,36 @@
 #include <curses.h> /* ncurses.h incluye stdio.h */
 #include <pthread.h>
+#include <signal.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
 
-typedef struct
-{
+// Global Variables
+typedef struct {
   int x;
   int y;
 } BallPosition;
 
 #define ENTER_NCURSES 10
-int mode = 0; // Mode 0 = Automatic. Mode 1 = Manual
+int mode = 0;  // Mode 0 = Automatic. Mode 1 = Manual
 int counter_val = 0;
 int row;
 int col;
-void * keyboardInputs();
-void * ballPlotter();
+void* keyboardInputs();  // Function used in thread to read user inputs
+void* ballPlotter();     // Function used in thread to plot ball movement
 BallPosition ball_pos;
 
-int main()
-{
+/**
+ * @brief Program that launches a ball around a stage, which bounces around.
+ *
+ * @details Utilizing ncurses, we made a program which launches a ball around a
+ * set stage. The ball can move around in two modes: automatic and manual. In
+ * automatic mode, the ball moves diagonally throughout the stage, colliding
+ * with its borders. In manual mode, the user can use the arrow keys to move the
+ * ball around to where they please. To switch between these modes, the spacebar
+ * can be pressed. A thread is made to plot the movement of the ball, and
+ * another one used to capture the inputs of the user.
+ */
+int main() {
   initscr();
   keypad(stdscr, TRUE);
   noecho();
@@ -35,32 +45,29 @@ int main()
   pthread_attr_t attr;
   pthread_attr_init(&attr);
 
-  pthread_create(&inputThread,&attr,keyboardInputs,NULL);
-  pthread_create(&plotterThread,&attr,ballPlotter,NULL);
+  // Start the threads
+  pthread_create(&inputThread, &attr, keyboardInputs, NULL);
+  pthread_create(&plotterThread, &attr, ballPlotter, NULL);
 
-  while (1)
-  {
-    if(mode==0){
+  // While loop in main checks for the collisions and
+  // what to do in each case.
+  while (1) {
+    if (mode == 0) {
       ball_pos.x += dx;
       ball_pos.y += dy;
-      if (ball_pos.x >= col - 1)
-      {
+      if (ball_pos.x >= col - 1) {
         dx = -1;
       }
-      if (ball_pos.y >= row - 1)
-      {
+      if (ball_pos.y >= row - 1) {
         dy = -1;
       }
-      if (ball_pos.x <= 1)
-      {
+      if (ball_pos.x <= 1) {
         dx = 1;
       }
-      if (ball_pos.y <= 1)
-      {
+      if (ball_pos.y <= 1) {
         dy = 1;
       }
-    }
-    else if(mode==1){
+    } else if (mode == 1) {
       //
     }
 
@@ -73,75 +80,71 @@ int main()
   return 0;
 }
 
-void * keyboardInputs(){
+void* keyboardInputs() {
   nodelay(stdscr, TRUE);
   int key_stroke;
   bool break_loop = false;
-  while(!break_loop)
-  {
+  while (!break_loop) {
     key_stroke = getch();
-    switch (key_stroke)
-    {
-    case KEY_UP:
-      /* code */
-      if(mode==1){
-        if(ball_pos.y>=1){
-          ball_pos.y--;
+    switch (key_stroke) {
+      case KEY_UP:
+        /*Make the ball move up */
+        if (mode == 1) {
+          if (ball_pos.y >= 1) {
+            ball_pos.y--;
+          }
         }
-      }
-      break;
-    case KEY_DOWN:
-      /* code */
-      if(mode==1){
-        if(ball_pos.y<row-1){
-          ball_pos.y++;
+        break;
+      case KEY_DOWN:
+        /* Make the ball move down */
+        if (mode == 1) {
+          if (ball_pos.y < row - 1) {
+            ball_pos.y++;
+          }
         }
-      }
-      break;
-    case KEY_LEFT:
-      /* code */
-      if(mode==1){
-        if(ball_pos.x>=1){
-          ball_pos.x--;
+        break;
+      case KEY_LEFT:
+        /* Make the ball move left */
+        if (mode == 1) {
+          if (ball_pos.x >= 1) {
+            ball_pos.x--;
+          }
         }
-      }
-      break;
-    case KEY_RIGHT:
-      /* code */
-      if(mode==1){
-        if(ball_pos.x<col-1){
-          ball_pos.x++;
+        break;
+      case KEY_RIGHT:
+        /* Make the ball move right */
+        if (mode == 1) {
+          if (ball_pos.x < col - 1) {
+            ball_pos.x++;
+          }
         }
-      }
-      break;
-    case 32:
-      //
-      if(mode==0){
-        mode = 1;
-      }
-      else{
-        mode = 0;
-      }
-      break;
-    case KEY_BACKSPACE:
-      /* code */
-      kill(getpid(),SIGTERM);
+        break;
+      case 32:
+        // Spacebar press. Switch Modes
+        if (mode == 0) {
+          mode = 1;
+        } else {
+          mode = 0;
+        }
+        break;
+      case KEY_BACKSPACE:
+        /* Backspace kills the program */
+        kill(getpid(), SIGTERM);
     }
-    usleep(100000); // Small delay to avoid high CPU usage
+    usleep(100000);  // Small delay to avoid high CPU usage
   }
 }
 
-void * ballPlotter(){
-  while(1){
+void* ballPlotter() {
+  while (1) {
     clear();
     getmaxyx(stdscr, row, col); /* Obtiene el numbero de filas y columnas */
     char mode_name[20];
 
-    if(mode==0){
-      sprintf(mode_name,"%s","Automatic");
-    }
-    else{
-      sprintf(mode_name,"%s","Manual");
+    if (mode == 0) {
+      sprintf(mode_name, "%s", "Automatic");
+    } else {
+      sprintf(mode_name, "%s", "Manual");
     }
     mvprintw(0, 0, "%d", counter_val++);
     mvprintw(1, 0, "%s", mode_name);
